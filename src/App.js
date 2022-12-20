320afdd46551a37d9962f6e05bf91692f7103d6e
dazenk <kevin.clavijo984@gmail.com>
2022-12-20 14:19:08 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Mensaje from "./Mensaje";
 
-function App() {
+const Description = () => {
+  return <p>Esta es la app del curso fullstack bootcamp</p>;
+};
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Mensaje color="yellow" message="Estamos trabajando" />
+      <Mensaje color="blue" message="En un curso" />
+      <Mensaje color="red" message="De React" />
+      <Description />
     </div>
   );
-}
+};
 
 export default App;
